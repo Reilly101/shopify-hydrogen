1466c6024b1ebc0f88c23c95ea4e772f6c6fbc72
Reilly101 <99052989+Reilly101@users.noreply.github.com>
2023-01-30 12:05:13 -0800

    1 file modified

--- src/components/global/Header.client.jsx
+++ src/components/global/Header.client.jsx
@@ -68,15 +68,8 @@ function MobileHeader({ countryCode, title, isHome, openCart, openMenu }) {
 
   const styles = {
     // button: 'relative flex items-center justify-center w-8 h-8',
-    container: `${
-      // isHome
-        // ? 'bg-primary/80 dark:bg-contrast/60 text-contrast dark:text-primary shadow-darkHeader'
-        : 'bg-contrast/80 text-primary'
-    } ${
-      y > 50  {'shadow-lightHeader '}
-    `flex lg hidden items-center h-nav sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 px-4 md:px-8`
+    container: `bg-contrast/80 text-primary flex lg hidden items-center h-nav sticky backdrop-blur-lg z-40 top-0 justify-between w-full leading-none gap-4 px-4 md:px-8`,
   };
-
   return (
     <header role="banner" className={styles.container}>
       <div className="flex items-center justify-start w-full gap-4">
